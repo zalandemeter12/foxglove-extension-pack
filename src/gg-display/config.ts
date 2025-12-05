eb77e3ea63446ff7d633b86fc013081c51d7a169
Zalán Demeter <zalandemeter12@gmail.com>
2025-12-05 11:59:43 +0100

    update dependencies

--- src/gg-display/config.ts
+++ src/gg-display/config.ts
@@ -2,11 +2,14 @@ const { VueLoaderPlugin } = require("vue-loader");
 
 module.exports = {
   webpack: (config) => {
+    config.module = config.module || {};
+    config.module.rules = config.module.rules || [];
     config.module.rules.push({
       test: /\.vue$/,
       loader: "vue-loader",
     });
 
+    config.plugins = config.plugins || [];
     config.plugins.push(new VueLoaderPlugin());
 
     return config;
