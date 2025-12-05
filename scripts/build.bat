d3d91bcdca34aac1e717d7eec8af1b7c08610308
Zalán Demeter <zalandemeter12@gmail.com>
2025-12-05 15:16:03 +0100

    fix win build script again

--- scripts/build.bat
+++ scripts/build.bat
@@ -9,34 +9,52 @@ set "orig_dir=%cd%"
 
 if exist "%target_file%" (
     for /f "usebackq tokens=* delims= " %%t in ("%target_file%") do (
-        call :process_line "%%t"
-    )
-) else (
-    for %%t in (circular-gauge value-display gg-display) do (
-        call :process_line "%%t"
-    )
-)
-
-goto :eof
-
-:process_line
-set "line=%~1"
-REM skip empty lines and lines starting with #
-if defined line (
-    set "firstchar=!line:~0,1!"
-    if not "!firstchar!"=="#" (
-        if not "!line!"=="" (
-            REM Exclude the 'common' target (case-insensitive)
-            if /I "!line!"=="common" (
-                echo Skipping common target
-            ) else (
-                echo Building !line!...
-                cd /d "%orig_dir%\src\!line!"
-                npm run local-install
-                npm run package
-                cd /d "%orig_dir%"
+        set "target=%%t"
+        REM skip empty lines and lines starting with #
+        if defined target (
+            set "firstchar=!target:~0,1!"
+            if not "!firstchar!"=="#" (
+                if not "!target!"=="" (
+                    REM Exclude the 'common' target (case-insensitive)
+                    if /I not "!target!"=="common" (
+                        echo Building !target!...
+                        if exist "%orig_dir%\src\!target!" (
+                            pushd "%orig_dir%\src\!target!"
+                            call npm run local-install
+                            if !errorlevel! equ 0 (
+                                call npm run package
+                            )
+                            popd
+                        ) else (
+                            echo Warning: Directory "%orig_dir%\src\!target!" does not exist
+                        )
+                    ) else (
+                        echo Skipping common target
+                    )
+                )
             )
         )
     )
+) else (
+    for %%t in (circular-gauge value-display gg-display) do (
+        set "target=%%t"
+        REM Exclude the 'common' target (case-insensitive)
+        if /I not "!target!"=="common" (
+            echo Building !target!...
+            if exist "%orig_dir%\src\!target!" (
+                pushd "%orig_dir%\src\!target!"
+                call npm run local-install
+                if !errorlevel! equ 0 (
+                    call npm run package
+                )
+                popd
+            ) else (
+                echo Warning: Directory "%orig_dir%\src\!target!" does not exist
+            )
+        ) else (
+            echo Skipping common target
+        )
+    )
 )
-goto :eof
+
+endlocal
